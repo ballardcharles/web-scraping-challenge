da973bd4a304203ddf654004251f6e556ef61a9d
ballardcharles <73306975+ballardcharles@users.noreply.github.com>
2021-03-10 20:21:37 -0500

    removed duplicate file.

--- mars_data_input.py
+++
@@ -1,8 +0,0 @@
-# Setup connection to mongodb
-conn = "mongodb://localhost:27017"
-client = pymongo.MongoClient(conn)
-
-# Select database and collection to use
-db = client.mars_db
-mars = db.mars
-
